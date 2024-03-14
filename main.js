eba6a039068d565b20966b8303dfa412c1b965e1
Isha Pervez <162013997+ishapervez11@users.noreply.github.com>
2024-03-14 11:47:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var num1 = 22;
+var num2 = 2;
+//addition
+console.log(num1 + num2);
+//subtraction
+console.log(num1 - num2);
+//multiplication
+console.log(num1 * num2);
+//division
+console.log(num1 / num2);
+//exponentation
+console.log(Math.pow(num1, num2));
+//modulus
+console.log(num1 % num2);
---
+++ main.ts
@@ -0,0 +1,20 @@
+let num1:number=22
+let num2:number=2
+
+//addition
+console.log(num1+num2)
+
+//subtraction
+console.log(num1-num2)
+
+//multiplication
+console.log(num1*num2)
+
+//division
+console.log(num1/num2)
+
+//exponentation
+console.log(num1**num2)
+
+//modulus
+console.log(num1%num2)
